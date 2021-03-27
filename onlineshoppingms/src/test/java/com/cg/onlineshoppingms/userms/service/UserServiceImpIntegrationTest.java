aba31275926be36420599d0113a2a0d677867340
jatint17 <jatintakkar@ymail.com>
2021-03-27 11:47:12 +0530

    Integration and Unit tests updated

--- onlineshoppingms/src/test/java/com/cg/onlineshoppingms/userms/service/UserServiceImpIntegrationTest.java
+++ onlineshoppingms/src/test/java/com/cg/onlineshoppingms/userms/service/UserServiceImpIntegrationTest.java
@@ -1,158 +1,195 @@
-//package com.cg.onlineshoppingms.userms.service;
-//
-//import com.cg.onlineshoppingms.userms.entity.User;
-//import com.cg.onlineshoppingms.userms.exceptions.InvalidPasswordException;
-//import com.cg.onlineshoppingms.userms.exceptions.InvalidUsernameException;
-//import org.junit.jupiter.api.Assertions;
-//import org.junit.jupiter.api.Test;
-//import org.junit.jupiter.api.extension.ExtendWith;
-//import org.junit.jupiter.api.function.Executable;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
-//import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-//import org.springframework.context.annotation.Import;
-//import org.springframework.test.context.junit.jupiter.SpringExtension;
-//import javax.persistence.EntityManager;
-//import java.util.List;
-//
-//import static org.junit.jupiter.api.Assertions.*;
-//import static org.junit.jupiter.api.Assertions.assertThrows;
-//
-//@ExtendWith(SpringExtension.class)
-//@Import(UserServiceImpl.class)
-//@DataJpaTest
-//@AutoConfigureTestDatabase
-//public class UserServiceImpIntegrationTest
-//{
-//
-//    @Autowired
-//    UserServiceImpl userService;
-//    @Autowired
-//    EntityManager entityManager;
-//
+package com.cg.onlineshoppingms.userms.service;
+
+import com.cg.onlineshoppingms.userms.entity.User;
+import com.cg.onlineshoppingms.userms.exceptions.InvalidUsernameException;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.api.function.Executable;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.context.annotation.Import;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import javax.persistence.EntityManager;
+import java.util.HashSet;
+import java.util.Set;
+
+@ExtendWith(SpringExtension.class)
+@Import(UserServiceImpl.class)
+@DataJpaTest
+@AutoConfigureTestDatabase
+public class UserServiceImpIntegrationTest
+{
+
+    @Autowired
+    UserServiceImpl userService;
+    @Autowired
+    EntityManager entityManager;
+
+    /*
+     * user added successfully
+     */
+//    @Test
+//    public void testAddUser_1() {
+//        String username = "arpit";
+//        String password = "password";
+//        User result = userService.addUser(username, password);
+//        assertNotNull(result);
+//        List<User> users = entityManager.createQuery("from User", User.class).getResultList();
+//        assertEquals(1,users.size());
+//        User stored = users.get(0);
+//        assertEquals(stored.getUserId(), result.getUserId());
+//        assertEquals(username, result.getUsername());
+//        assertEquals(username, stored.getUsername());
+//        assertEquals(password, result.getPassword());
+//        assertEquals(password, stored.getPassword());
+//    }
 //    /*
-//     * user added successfully
-//     */
-////    @Test
-////    public void testAddUser_1() {
-////        String username = "arpit";
-////        String password = "password";
-////        User result = userService.addUser(username, password);
-////        assertNotNull(result);
-////        List<User> users = entityManager.createQuery("from User", User.class).getResultList();
-////        assertEquals(1,users.size());
-////        User stored = users.get(0);
-////        assertEquals(stored.getUserId(), result.getUserId());
-////        assertEquals(username, result.getUsername());
-////        assertEquals(username, stored.getUsername());
-////        assertEquals(password, result.getPassword());
-////        assertEquals(password, stored.getPassword());
-////    }
-////    /*
-////     * username is blank
-////     */
-////    @Test
-////    public void testAddUser_2() {
-////        String username = "";
-////        String password = "password";
-////        Executable executable=()-> userService.addUser(username, password);
-////        assertThrows(InvalidUsernameException.class, executable);
-////    }
-////
-////    /*
-////     * password is blank
-////     */
-////    @Test
-////    public void testAddUser_3() {
-////        String username = "arpit";
-////        String password = "";
-////        Executable executable=()-> userService.addUser(username, password);
-////        assertThrows(InvalidPasswordException.class, executable);
-////    }
-////
-////    /*
-////     * username is null
-////     */
-////    @Test
-////    public void testAddUser_4() {
-////        String username = null;
-////        String password = "password";
-////        Executable executable=()-> userService.addUser(username, password);
-////        assertThrows(InvalidUsernameException.class, executable);
-////    }
-////
-////    /*
-////     * password is blank
-////     */
-////    @Test
-////    public void testAddUser_5() {
-////        String username = "arpit";
-////        String password = null;
-////        Executable executable=()-> userService.addUser(username, password);
-////        assertThrows(InvalidPasswordException.class, executable);
-////    }
-//
-//    /**
-//     * Scenario: Username is empty
+//     * username is blank
 //     */
 //    @Test
-//    public void testCheckCredentials_1() {
+//    public void testAddUser_2() {
 //        String username = "";
 //        String password = "password";
-//        boolean result = userService.checkCredentials(username, password);
-//        Assertions.assertFalse(result);
+//        Executable executable=()-> userService.addUser(username, password);
+//        assertThrows(InvalidUsernameException.class, executable);
 //    }
 //
-//    /**
-//     * Scenario: Password is empty
+//    /*
+//     * password is blank
 //     */
 //    @Test
-//    public void testCheckCredentials_2() {
-//        String username = "user";
+//    public void testAddUser_3() {
+//        String username = "arpit";
 //        String password = "";
-//        boolean result = userService.checkCredentials(username, password);
-//        Assertions.assertFalse(result);
+//        Executable executable=()-> userService.addUser(username, password);
+//        assertThrows(InvalidPasswordException.class, executable);
 //    }
 //
-//    /**
-//     * Scenario: username does not match the database
+//    /*
+//     * username is null
 //     */
 //    @Test
-//    public void testCheckCredentials_3() {
-//        String username = "user";
+//    public void testAddUser_4() {
+//        String username = null;
 //        String password = "password";
-//        String enteredUsername = "wrong";
-//        User user = new User(username, password);
-//        entityManager.persist(user);
-//        boolean result = userService.checkCredentials(enteredUsername, password);
-//        Assertions.assertFalse(result);
+//        Executable executable=()-> userService.addUser(username, password);
+//        assertThrows(InvalidUsernameException.class, executable);
 //    }
 //
-//    /**
-//     * Scenario: password does not match the database
+//    /*
+//     * password is blank
 //     */
 //    @Test
-//    public void testCheckCredentials_4() {
-//        String username = "user";
-//        String password = "password";
-//        String enteredPassword = "wrong";
-//        User user = new User(username, password);
-//        entityManager.persist(user);
-//        boolean result = userService.checkCredentials(username, enteredPassword);
-//        Assertions.assertFalse(result);
+//    public void testAddUser_5() {
+//        String username = "arpit";
+//        String password = null;
+//        Executable executable=()-> userService.addUser(username, password);
+//        assertThrows(InvalidPasswordException.class, executable);
 //    }
-//
-//    /**
-//     * Scenario: credentials are matching
-//     */
-//    @Test
-//    public void testCheckCredentials_5()
-//    {
-//        String username = "user";
-//        String password = "password";
-//        User user = new User(username,password);
-//        entityManager.persist(user);
-//        boolean result = userService.checkCredentials(username,password);
-//        Assertions.assertTrue(result);
-//    }
-//}
+
+    /**
+     * Scenario: Username is empty
+     */
+    @Test
+    public void testCheckCredentials_1() {
+        String username = "";
+        String password = "password";
+        boolean result = userService.checkCredentials(username, password);
+        Assertions.assertFalse(result);
+    }
+
+    /**
+     * Scenario: Password is empty
+     */
+    @Test
+    public void testCheckCredentials_2() {
+        String username = "user";
+        String password = "";
+        boolean result = userService.checkCredentials(username, password);
+        Assertions.assertFalse(result);
+    }
+
+    /**
+     * Scenario: username does not match the database
+     */
+    @Test
+    public void testCheckCredentials_3() {
+        String username = "user";
+        String password = "password";
+        String enteredUsername = "wrong";
+        Set<String> roles = new HashSet<>();
+        roles.add("role1");
+        User user = new User(username, password, roles);
+        entityManager.persist(user);
+        boolean result = userService.checkCredentials(enteredUsername, password);
+        Assertions.assertFalse(result);
+    }
+
+    /**
+     * Scenario: password does not match the database
+     */
+    @Test
+    public void testCheckCredentials_4() {
+        String username = "user";
+        String password = "password";
+        String enteredPassword = "wrong";
+        Set<String> roles = new HashSet<>();
+        roles.add("role1");
+        User user = new User(username, password, roles);
+        entityManager.persist(user);
+        boolean result = userService.checkCredentials(username, enteredPassword);
+        Assertions.assertFalse(result);
+    }
+
+    /**
+     * Scenario: credentials are matching
+     */
+    @Test
+    public void testCheckCredentials_5()
+    {
+        String username = "user";
+        String password = "password";
+        Set<String> roles = new HashSet<>();
+        roles.add("role1");
+        User user = new User(username, password, roles);
+        entityManager.persist(user);
+        boolean result = userService.checkCredentials(username,password);
+        Assertions.assertTrue(result);
+    }
+
+    /**
+     * Scenario: user exists
+     */
+    @Test
+    public void testFindUserByUsername_1()
+    {
+        String username = "user";
+        String password = "password";
+        Set<String> roles = new HashSet<>();
+        roles.add("role1");
+        User saved = new User(username,password,roles);
+        entityManager.persist(saved);
+        User result = userService.findUserByUsername(username);
+        Assertions.assertNotNull(result);
+        Assertions.assertEquals(saved,result);
+    }
+
+    /**
+     * Scenario: user does not exist
+     */
+    @Test
+    public void testFindUserByUsername_2()
+    {
+        String username = "user";
+        String password = "password";
+        String enteredUsername = "wrong";
+        Set<String> roles = new HashSet<>();
+        roles.add("role1");
+        User user = new User(username, password, roles);
+        entityManager.persist(user);
+        Executable executable = ()-> userService.findUserByUsername(enteredUsername);
+        Assertions.assertThrows(InvalidUsernameException.class,executable);
+    }
+}
